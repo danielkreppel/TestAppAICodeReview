7b583813b3e81c7b08e4919a6706aace7f2fa568
unknown <dkreppel@gmail.com>
2024-09-20 15:17:02 -0300

    Test for pull request AI review

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,7 +2,7 @@ namespace TestAppAICodeReview.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
     }
